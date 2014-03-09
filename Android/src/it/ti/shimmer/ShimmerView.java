a53c5f974505877543ba9475ccdfc6db649b299d
Pier Paolo Ramon <ramonpierre@gmail.com>
2014-03-09 03:08:50 +0100

    Initial animation support

--- Android/src/it/ti/shimmer/ShimmerView.java
+++ Android/src/it/ti/shimmer/ShimmerView.java
@@ -1,40 +1,42 @@
 package it.ti.shimmer;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.DrawFilter;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
-import android.graphics.RectF;
-import android.graphics.Region;
 import android.graphics.Shader;
 import android.util.AttributeSet;
-import android.widget.LinearLayout;
 
 import org.appcelerator.titanium.TiC;
 import org.appcelerator.titanium.proxy.TiViewProxy;
-import org.appcelerator.titanium.util.TiColorHelper;
 import org.appcelerator.titanium.util.TiConvert;
-import org.appcelerator.titanium.util.TiUIHelper;
 import org.appcelerator.titanium.view.TiCompositeLayout;
 import org.appcelerator.titanium.view.TiUIView;
 
 public class ShimmerView extends TiUIView {
 
-    private TiCompositeLayout view;
+    protected TiCompositeLayout view;
+    protected ObjectAnimator maskAnimator;
+    protected ObjectAnimator introAnimator;
+
+    protected float maskX = 0.0f;
+    protected float maskFactor = 0.0f;
+
+    protected long introDuration = 300;
+    protected long shimDuration = 9000;
+
+    protected boolean shimming = false;
 
     public static final String TAG = "Shimmer";
 
     public ShimmerView(final TiViewProxy proxy) {
         super(proxy);
-
-        final ShimmerView that = this;
 
         TiCompositeLayout.LayoutArrangement arrangement =
             TiCompositeLayout.LayoutArrangement.DEFAULT;
@@ -50,11 +52,91 @@ public class ShimmerView extends TiUIView {
 
         view = new ShimmerCompositeLayout(proxy.getActivity(), arrangement, proxy);
 
+        maskAnimator = ObjectAnimator.ofFloat(this, "maskX", 0.0f, 1.0f);
+        maskAnimator.setDuration(shimDuration);
+        maskAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        maskAnimator.addListener(new Animator.AnimatorListener() {
+
+            @Override
+            public void onAnimationStart(Animator animator) {
+                shimming = true;
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                shimming = false;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+                //
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+                //
+            }
+        });
+
+        introAnimator = ObjectAnimator.ofFloat(this, "maskFactor", 1.0f);
+        introAnimator.setDuration(introDuration);
+        introAnimator.setRepeatCount(0);
+        introAnimator.addListener(new Animator.AnimatorListener() {
+
+            @Override
+            public void onAnimationStart(Animator animator) {
+                if (!shimming) {
+                    maskAnimator.start();
+                }
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                if (maskFactor == 0) {
+                    maskAnimator.end();
+                }
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+                //
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+                //
+            }
+        });
+
         setNativeView(view);
     }
 
-    protected class MyDrawFilter extends DrawFilter {
+    public void startShimming() {
+        introAnimator.setFloatValues(1.0f);
+        introAnimator.start();
+    }
 
+    public void endShimming() {
+        introAnimator.setFloatValues(0.0f);
+        introAnimator.start();
+    }
+
+    public void setMaskFactor(float maskFactor) {
+        this.maskFactor = maskFactor;
+        view.invalidate();
+    }
+
+    public void setMaskX(float maskX) {
+        this.maskX = maskX;
+        view.invalidate();
+    }
+
+    public float getMaskFactor() {
+        return maskFactor;
+    }
+
+    public float getMaskX() {
+        return maskX;
     }
 
     protected class ShimmerCompositeLayout extends TiCompositeLayout {
@@ -63,7 +145,8 @@ public class ShimmerView extends TiUIView {
             super(context);
         }
 
-        public ShimmerCompositeLayout(Context context, TiCompositeLayout.LayoutArrangement arrangement) {
+        public ShimmerCompositeLayout(
+                Context context, TiCompositeLayout.LayoutArrangement arrangement) {
             super(context, arrangement);
         }
 
@@ -75,45 +158,40 @@ public class ShimmerView extends TiUIView {
             super(context, proxy);
         }
 
-        public ShimmerCompositeLayout(Context context, TiCompositeLayout.LayoutArrangement arrangement, TiViewProxy proxy) {
+        public ShimmerCompositeLayout(
+                Context context, TiCompositeLayout.LayoutArrangement arrangement,
+                TiViewProxy proxy) {
             super(context, arrangement, proxy);
         }
 
         @Override
         public void dispatchDraw(Canvas canvas) {
-            System.out.println("dispatchDraw");
+            if (maskFactor > 0) {
+                Paint paint = new Paint();
 
-            /*Bitmap temporaryBitmap =
-                Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
+                paint.setFilterBitmap(false);
 
-            Canvas temporaryCanvas = new Canvas(temporaryBitmap);
+                int highColor = Color.argb(clamp(0xFF, 0x70, maskFactor), 255, 255, 255);
+                int lowColor = Color.argb(clamp(0xFF, 0x00, maskFactor), 255, 255, 255);
 
-            super.dispatchDraw(temporaryCanvas);*/
+                Shader shader = new LinearGradient(
+                        0, 0, 100 + maskX * 100, 0, highColor, lowColor,
+                        Shader.TileMode.REPEAT);
 
-            Paint paint = new Paint();
+                paint.setShader(shader);
+                paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
 
-            paint.setFilterBitmap(false);
+                super.dispatchDraw(canvas);
 
-            Shader shader = new LinearGradient(
-                0, 0, canvas.getWidth(), 0, 0x70ffffff, 0x00ffffff,
-                Shader.TileMode.REPEAT);
-
-            paint.setShader(shader);
-            paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-
-            /*canvas.drawBitmap(temporaryBitmap, 0, 0, null);*/
-
-            super.dispatchDraw(canvas);
-
-            canvas.drawPaint(paint);
+                canvas.drawPaint(paint);
+            }
+            else {
+                super.dispatchDraw(canvas);
+            }
         }
 
-        @Override
-        protected void onDraw(Canvas canvas) {
-            System.out.println("onDraw");
-            super.onDraw(canvas);
+        public int clamp(int from, int to, float factor) {
+            return Math.round(from * (1 - factor) + to * factor);
         }
-
     }
-
 }
--- Android/src/it/ti/shimmer/ShimmerViewProxy.java
+++ Android/src/it/ti/shimmer/ShimmerViewProxy.java
@@ -1,14 +1,23 @@
 package it.ti.shimmer;
 
 import android.app.Activity;
+import android.os.Message;
 
+import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.annotations.Kroll;
+import org.appcelerator.kroll.common.AsyncResult;
+import org.appcelerator.kroll.common.TiMessenger;
+import org.appcelerator.titanium.TiApplication;
 import org.appcelerator.titanium.TiContext;
 import org.appcelerator.titanium.proxy.TiViewProxy;
 import org.appcelerator.titanium.view.TiUIView;
 
 @Kroll.proxy(creatableInModule=ShimmerModule.class, propertyAccessors = {})
 public class ShimmerViewProxy extends TiViewProxy {
+
+    private static final int MSG_FIRST_ID = TiViewProxy.MSG_LAST_ID + 1;
+    private static final int MSG_START_SHIMMING = MSG_FIRST_ID + 900;
+    private static final int MSG_END_SHIMMING = MSG_FIRST_ID + 901;
 
     public ShimmerViewProxy() {
         super();
@@ -21,6 +30,81 @@ public class ShimmerViewProxy extends TiViewProxy {
     @Override
     public TiUIView createView(Activity activity) {
         return new ShimmerView(this);
+    }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        switch (msg.what) {
+            case MSG_START_SHIMMING: {
+                AsyncResult result = (AsyncResult)msg.obj;
+                handleStartShimming();
+                result.setResult(null);
+                return true;
+            }
+
+            case MSG_END_SHIMMING: {
+                AsyncResult result = (AsyncResult)msg.obj;
+                handleEndShimming();
+                result.setResult(null);
+                return true;
+            }
+
+            default: {
+                return super.handleMessage(msg);
+            }
+        }
+    }
+
+    @Kroll.method
+    public void startShimming() {
+        System.out.println("startShimming?");
+        if (TiApplication.isUIThread()) {
+            handleStartShimming();
+        }
+        else {
+            System.out.println("startShimming? Not UI");
+            TiMessenger.sendBlockingMainMessage(getMainHandler().obtainMessage(MSG_START_SHIMMING));
+        }
+    }
+
+    @Kroll.method
+    public void endShimming() {
+        System.out.println("endShimming?");
+        if (TiApplication.isUIThread()) {
+            handleEndShimming();
+        }
+        else {
+            System.out.println("endShimming? Not UI");
+            TiMessenger.sendBlockingMainMessage(getMainHandler().obtainMessage(MSG_END_SHIMMING));
+        }
+    }
+
+    @Kroll.method
+    public void stopShimming() {
+        System.out.println("stopShimming?");
+        endShimming();
+    }
+
+    public void handleStartShimming() {
+        TiUIView view = peekView();
+
+        if (view != null) {
+            ((ShimmerView)view).startShimming();
+        }
+        else {
+            System.out.println(" handleStartShimming -- whaat? ");
+        }
+    }
+
+    public void handleEndShimming() {
+        TiUIView view = peekView();
+
+        if (view != null) {
+            ((ShimmerView)view).endShimming();
+        }
+        else {
+            System.out.println(" handleEndShimming -- whaat? ");
+        }
     }
 
     @Override
