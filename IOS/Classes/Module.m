03240a1a447b7fab5f6ee72df31d64f4236f1a62
Pier Paolo Ramon <ramonpierre@gmail.com>
2014-03-23 19:03:24 +0100

    Renaming

--- IOS/Classes/Module.m
+++ IOS/Classes/Module.m
@@ -4,7 +4,7 @@
  * Appcelerator Titanium is Copyright (c) 2009-2010 by Appcelerator, Inc.
  * and licensed under the Apache Public License (version 2)
  */
-#import "ItScsoftShimmerModule.h"
+#import "Module.h"
 #import "TiBase.h"
 #import "TiHost.h"
 #import "TiUtils.h"
